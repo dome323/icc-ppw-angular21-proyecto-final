95b3ef1d99cedbedfd762b0b02e46d03ef21f677
dome323 <domeuyunkar@gmail.com>
2026-05-26 23:11:41 -0500

    estructura inicial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { HomePage } from './features/home/pages/home-page/home-page';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomePage,
+  },
+];
---
+++ src/app/features/gallery/pages/gallery-page/gallery-page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gallery-page',
+  imports: [],
+  templateUrl: './gallery-page.html',
+  styleUrl: './gallery-page.css',
+})
+export class GalleryPage {}
