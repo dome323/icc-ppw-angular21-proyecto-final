7a01e9892d9350ff36c44177280fc0fcea769648
dome323 <domeuyunkar@gmail.com>
2026-06-03 11:15:49 -0500

    feat: Avance DOMENICA UYUNKAR

--- src/app/core/services/auth/auth.ts
+++ src/app/core/services/auth/auth.ts
@@ -3,12 +3,10 @@ import { initializeApp } from 'firebase/app';
 import { 
   getAuth, 
   signInWithEmailAndPassword, 
-  createUserWithEmailAndPassword, 
   signOut, 
   onAuthStateChanged,
   User 
 } from 'firebase/auth';
-
 import { BehaviorSubject, Observable } from 'rxjs';
 import { environment } from '../../../../enviroments/environment';
 
@@ -16,36 +14,27 @@ import { environment } from '../../../../enviroments/environment';
   providedIn: 'root'
 })
 export class AuthService {
-  // Inicializamos Firebase con nuestro environment
   private app = initializeApp(environment.firebaseConfig);
   private auth = getAuth(this.app);
 
-  // Un BehaviorSubject nos permite saber en tiempo real y desde cualquier componente si el usuario está logueado o no
   private user$ = new BehaviorSubject<User | null>(null);
 
   constructor() {
-    // Escuchar activamente los cambios en el estado de autenticación (si se loguea o desloguea)
     onAuthStateChanged(this.auth, (user) => {
       this.user$.next(user);
     });
   }
 
-  // Función para registrar un nuevo usuario
-  register(email: string, pass: string) {
-    return createUserWithEmailAndPassword(this.auth, email, pass);
-  }
-
-  // Función para iniciar sesión
+  // Lógica para iniciar sesión
   login(email: string, pass: string) {
     return signInWithEmailAndPassword(this.auth, email, pass);
   }
 
-  // Función para cerrar sesión
+  // Lógica para cerrar sesión
   logout() {
     return signOut(this.auth);
   }
 
-  // Obtener el estado del usuario como un Observable (para suscribirnos desde los componentes)
   get currentUser$(): Observable<User | null> {
     return this.user$.asObservable();
   }
